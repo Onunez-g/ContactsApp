66167c7efb2a9683ff76a453c03e5fa70f8af081
onunez <onunez@camsoft.com.do>
2020-02-25 00:23:33 -0400

    Tae and Pick Photo

---
+++ ContactsApp/ContactsApp/Models/FrameImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsApp.Models
+{
+    class FrameImage
+    {
+        public string CornerRadius { get; set; } = "100";
+        public string HeightRequest { get; set; } = "55";
+        public string WidthRequest { get; set; } = "55";
+        public string BackgroundColor { get; set; } = "#1A73E9";
+    }
+}
--- ContactsApp/ContactsApp/ViewModels/NewContactViewModel.cs
+++ ContactsApp/ContactsApp/ViewModels/NewContactViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using ContactsApp.Models;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -11,10 +14,12 @@ namespace ContactsApp.ViewModels
 {
     class NewContactViewModel : INotifyPropertyChanged
     {
-        public Contact Contact { get; set; } = new Contact() { NumberTag = "Celular", EmailTag = "Particular"};
+        public Contact Contact { get; set; } = new Contact() {Image = "AddPhoto", NumberTag = "Celular", EmailTag = "Particular"};
         public bool IsEdit { get; set; }
         public string Title { get; set; } = "New contact";
+        public FrameImage FrameImage { get; set; } = new FrameImage();
         public ICommand AddContactCommand { get; set; }
+        public ICommand AddContactPhotoCommand { get; set; }
         public List<string> NumberTags { get; set; }
         public List<string> EmailTags { get; set; }
         public NewContactViewModel(ObservableCollection<Contact> contacts, Contact contact = null)
@@ -22,6 +27,13 @@ namespace ContactsApp.ViewModels
             if (contact != null)
             {
                 Contact = contact;
+                FrameImage = FrameImage = new FrameImage()
+                {
+                    BackgroundColor = "Transparent",
+                    CornerRadius = "0",
+                    HeightRequest = "100",
+                    WidthRequest = "200"
+                };
                 IsEdit = true;
                 Title = "Edit contact";
             }
@@ -37,6 +49,19 @@ namespace ContactsApp.ViewModels
                     contacts.Add(Contact);
                 await App.Current.MainPage.Navigation.PopAsync();
             });
+            AddContactPhotoCommand = new Command(async () =>
+            {
+                string option = await App.Current.MainPage.DisplayActionSheet("Change photo", "Cancel", "", "Take photo", "Pick photo");
+                switch (option)
+                {
+                    case "Take photo":
+                        TakePhoto();
+                        break;
+                    case "Pick photo":
+                        PickPhoto();
+                        break;
+                }
+            });
             NumberTags = new List<string>()
             {
                 "Celular",
@@ -51,7 +76,48 @@ namespace ContactsApp.ViewModels
                 "Otro"
             };
         }
+        private async void TakePhoto()
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await App.Current.MainPage.DisplayAlert("No Camera", "No camera available.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+            if (file == null)
+                return;
+            Contact.Image = file.Path;
+            FrameImage = new FrameImage()
+            {
+                BackgroundColor = "Transparent",
+                CornerRadius = "0",
+                HeightRequest = "100",
+                WidthRequest = "200"
+            };
+        }
+        private async void PickPhoto()
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await App.Current.MainPage.DisplayAlert("Photos Not Supported", "Permission not granted to photos.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.PickPhotoAsync().ConfigureAwait(true);
 
+            if (file == null)
+                return;
+            Contact.Image = file.Path;
+            file.Dispose();
+            FrameImage = new FrameImage()
+            {
+                BackgroundColor = "Transparent",
+                CornerRadius = "0",
+                HeightRequest = "100",
+                WidthRequest = "200"
+            };
+        }
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
